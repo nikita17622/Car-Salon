033159bf95d7565fe720003e44eced5ac8c7f77c
nikita17622 <kovalenkovnikita7@gmail.com>
2026-06-25 22:56:48 +0300

    Добавлен наследуемый класс Moto

--- CarSalon/Car.cs
+++ CarSalon/Car.cs
@@ -8,5 +8,27 @@ namespace CarSalon
 {
     internal class Car
     {
+            public int Number { get; set; }
+            public string Name { get; set; }
+            public decimal Cost { get; set; }
+            public int Year_Creation { get; set; }
+            public int Speed { get; set; }
+            public string FuelType { get; set; }
+
+            public Car(string name, decimal cost, int yearCreation, int speed, string fuelType)
+            {
+                Name = name;
+                Cost = cost;
+                Year_Creation = yearCreation;
+                Speed = speed;
+                FuelType = fuelType;
+            }
+
+            public override string ToString()
+            {
+                return $" №{Number}. Модель : {Name}, стоимость : {Cost:C}, год выпуска : {Year_Creation}, макс. скорость : {Speed}, тип топлива : {FuelType}";
+            }
+        
     }
 }
+
---
+++ CarSalon/Moto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSalon
+{
+    internal class Moto : Car
+    {
+        public int Quantity_Seat { get; set; }
+        public int Quantity_Horses { get; set; }
+
+        public Moto(string name, decimal cost, int yearCreation, int speed, string fuelType, 
+                    int quantitySeat, int quantityHorses)
+            : base(name, cost, yearCreation, speed, fuelType)
+        {
+            Quantity_Seat = quantitySeat;
+            Quantity_Horses = quantityHorses;
+        }
+
+        public override string ToString()
+        {
+            return $" №{Number}. Мотоцикл : {Name}, стоимость : {Cost:C}, год выпуска : {Year_Creation}, макс. скорость : {Speed}, тип топлива : {FuelType}, мест : {Quantity_Seat}, л.с. : {Quantity_Horses}";
+        }
+    }
+}
+    
+
